faebe23d3507ed88b6ffc5294ab5143fd61128a5
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-10 17:32:15 +0530

    data1

--- pages/eom.js
+++ pages/eom.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
-const EOM = () => {
+const EOM = ({employee}) => {
+    console.log(employee);
   return (
-    <div>eom</div>
+    <div>{employee.name}</div>
   )
 }
 
 export const getServerSideProps = async pageContext => {
-    
-}
+    const apiResponse = await fetch(
+        'https://my-json-server.typicode.com/sreerajsree/news-app/employeeOfTheMonth'
+    )
+    const employee = await apiResponse.json();
+
+    return {
+        props: {
+            employee : employee
+        },
+    };
+};
 
 export default EOM
