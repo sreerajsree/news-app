86f1faadd29f03257f646f44c20e6cd2249f1955
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-10 17:40:44 +0530

    data2

--- pages/eom.js
+++ pages/eom.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-const EOM = ({employee}) => {
-    console.log(employee);
+const EOM = ({ employee }) => {
+  console.log(employee);
   return (
-    <div>{employee.name}</div>
-  )
-}
-
-export const getServerSideProps = async pageContext => {
-    const apiResponse = await fetch(
-        'https://my-json-server.typicode.com/sreerajsree/news-app/employeeOfTheMonth'
-    )
-    const employee = await apiResponse.json();
-
-    return {
-        props: {
-            employee : employee
-        },
-    };
+    <>
+      <div>
+        <h1>About Dev</h1>
+      </div>
+      <div>
+        <p>{employee.name}</p>
+        <p>{employee.position}</p>
+        <Image src={employee.image} width={50} height={50} layout="responsive" />
+        <p>{employee.description}</p>
+      </div>
+    </>
+  );
 };
 
-export default EOM
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/sreerajsree/news-app/employeeOfTheMonth"
+  );
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee: employee,
+    },
+  };
+};
+
+export default EOM;
